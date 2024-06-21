3826b788b694c1deb6acd0f4e7aebe9648930abe
Pathanin12 <rungpathanin12@gmail.com>
2024-06-22 01:42:42 +0700

    auth

--- lib/api/authapi/checkapikey.dart
+++ lib/api/authapi/checkapikey.dart
@@ -1,23 +1,46 @@
-
-
 import 'package:dio/dio.dart';
+import 'package:disaster/router.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../model/profileusermodel.dart';
+import '../../screen/drawer/admin/contollerdraweradmin.dart';
 import '../../service/config.dart';
 
-Future<String?>checkApiKey(String key)async{
-  try{
-    Dio dio=Dio();
-    final result=await dio.post('${urlApiKey}valid?apikey=$key',options: Options(headers: {
-      "Content-Type":"application/json",
-      "Accept":"application/json"
-    }));
-    if(result.statusCode==200){
-      Map<String,dynamic> data = result.data;
-      print(data['token']);
-      return data['token'];
+Future<void> checkApiKey({String? keyApi}) async {
+  try {
+    String? key = '';
+    if (keyApi == null) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      key = prefs.getString('apikey');
+    } else {
+      key = keyApi;
     }
-  }catch(e){
+    Dio dio = Dio();
+    final result = await dio.post('${urlApiKey}valid?apikey=$key',
+        options: Options(headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }));
+    if (result.statusCode == 200) {
+      Map<String, dynamic> data = result.data;
+      final resultProfile = await dio.get('${urlApiKey}profile',
+          options: Options(headers: {
+            "Accept": "application/json",
+            "apikey": data['token']
+          }));
+
+      if (resultProfile.statusCode == 200) {
+        ProfileModel profile = ProfileModel.fromJson(resultProfile.data);
+        final LandingPageControllerAdmin landingPageController =
+            Get.put(LandingPageControllerAdmin(), permanent: false);
+        landingPageController.dataUserAdmin.value = profile;
+        dataUser = profile;
+        Get.toNamed(RouterName.adminPage);
+      }
+    }
+  } catch (e) {
     print('ERROR CheckApiKey => $e');
-    return null;
+    Get.toNamed(RouterName.userPage);
   }
 }
